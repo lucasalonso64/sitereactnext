d54d493b816a19a316d9874663e83ee4dd134c16
lucasalonso64 <alonsosistemas@gmail.com>
2020-04-22 19:12:00 -0300

    0.1

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import mongoose from 'mongoose';
+
+import UserController from './app/controllers/UserController';
+import LoginController from './app/controllers/LoginController';
+import PerfilController from './app/controllers/PerfilController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
+routes.delete('/users/:id', authMiddleware, UserController.delete);
+
+routes.get('/perfil', authMiddleware, PerfilController.show);
+routes.put('/perfil', authMiddleware, PerfilController.update);
+
+routes.post('/login', LoginController.store);
+
+export default routes;
